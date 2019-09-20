03b5f426a0d4d31a3bf4e10b19a9b75f9173e391
Oliver Day <oday@post.harvard.edu>
2019-09-20 14:13:52 -0400

    Add date, time, and length metadata.
    The date and time are embedded in the filename. The length of the file is an indication of the duration of the recording.

--- tag-read.rb
+++ tag-read.rb
@@ -7,13 +7,22 @@ require 'uri'
 
 mp3_file = ARGV[0]
 raw_data = File.open(mp3_file).read
+mp3_size = File.size?(mp3_file)
+
 base64_encoded_data = Base64.encode64(raw_data)
 TagLib::MPEG::File.open(mp3_file) do |file|
+  # timestamp is embedded in filename
+  dot, date, filename = mp3_file.split("/")
+  timestamp, wav, mp3 = filename.split(".")
+  date, time = timestamp.split("_")
   tag = file.id3v2_tag
   puts "title: #{tag.title}"
+  puts "length: #{mp3_size}"
   puts "artist: #{tag.artist}"
   puts "genre: #{tag.genre}"
   puts "year: #{tag.year}"
+  puts "date: #{date}"
+  puts "time: #{time}"
   puts "album: #{tag.album}"
   # curl -X PUT http://localhost:5984/test/docid 
   #      -d '{"_attachments": {"message.txt":
@@ -29,7 +38,10 @@ TagLib::MPEG::File.open(mp3_file) do |file|
                             "artist": "#{tag.artist}",
                             "genre": "#{tag.genre}",
                             "year": "#{tag.year}",
+                            "date": "#{date}",
+                            "time": "#{time}",
                             "album": "#{tag.album}",
+                            "length": "#{mp3_size}",
                             "_attachments" => {
                               mp3_file => {
                                 "data" => base64_encoded_data,
